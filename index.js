1d8a69a2412308cf21d74d77ef629c71128a2e8b
Hongyu Yang <hongyuy96@gmail.com>
2022-02-22 14:24:42 -0800

    init

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const { process_params } = require('express/lib/router');
+const app = express();
+
+app.get('/', (req,res) => {
+  res.send({hi: "there"})
+});
+// create a port
+const port  = process.env.PORT ||5010;
+app.listen(port);
+// localhost:5010
