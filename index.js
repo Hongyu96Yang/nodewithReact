86d3a3147129e7d3f87babc40eb9f85d1654df20
Hongyu Yang <hongyuy96@gmail.com>
2022-02-22 14:27:32 -0800

    try to update

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const { process_params } = require('express/lib/router');
+const express = require("express");
+const { process_params } = require("express/lib/router");
 const app = express();
 
-app.get('/', (req,res) => {
-  res.send({hi: "there"})
+app.get("/", (req, res) => {
+  res.send("very beginning of a project");
 });
 // create a port
-const port  = process.env.PORT ||5010;
+const port = process.env.PORT || 5010;
 app.listen(port);
 // localhost:5010
